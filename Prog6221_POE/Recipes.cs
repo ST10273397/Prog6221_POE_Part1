3ae31b1be9dfddd9fd6b7d6b291bc07017d02928
Nicholas Liam Wolfaardt <ST10273397@vcconnect.edu.za>
2024-04-16 15:50:28 +0200

    I have created multiole error exception handling for InputIngredeint, InputStep, CreateRecipe and GetScale. I have also created a new method that will return a string that has been made sure it has no numbers and is not blank or null, this is made for the Name of the ingredient and the name of the recipe. The StepDescription will no longer accept a blank string or a null variable. The GetScale method now gives the user another chance to enter a correct option.

--- Prog6221_POE/Recipes.cs
+++ Prog6221_POE/Recipes.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Net;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Prog6221_POE
 {
     internal class Recipes
     {
-        public string RecipeName {  get; set; }
+        public string RecipeName { get; set; }
 
-        public int NumOfIngredients {  get; set; }
+        public int NumOfIngredients { get; set; }
 
         public List<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
 
@@ -30,30 +32,56 @@ namespace Prog6221_POE
             RecipeName = recipeName;
             Ingredients = ingredients;
             NumOfIngredients = numOfIngredients;
-            Steps = steps;  
+            Steps = steps;
             NumOfSteps = stepNum;
             Scale = scale;
         }
 
         public void InputIngredients(int numOfIngredients)
         {
+            string name = null;
             Console.WriteLine("Please Enter the First Ingredient Name");
-            string name = Console.ReadLine();
+            name = StringCheck();
             Console.WriteLine("Please Enter the Unit of Measurement (whole/ml/cups/teaspoons)");
             string unit = Console.ReadLine();
             Console.WriteLine("How Much/Many of the Ingredient Will You Use? (Don't type in the Unit of Meaasurement)");
-            double quantity = Double.Parse(Console.ReadLine());
+            double quantity = 0;
+            while (quantity <= 0)
+            {
+                try
+                {
+                    quantity = Double.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{ex.Message}");
+                    Console.WriteLine("Please Type in a Number. No Letters, Words or Phrases.");
+                    continue;
+                }
+            }
             Ingredient newIngredient = new Ingredient { name = name, quantity = quantity, unit = unit, scale = 1 };
             Ingredients.Add(newIngredient);
             for (int i = 0; i < numOfIngredients - 1; i++)
             {
                 Console.WriteLine("Enter the Next Ingredient");
                 newIngredient = new Ingredient();
-                newIngredient.name = Console.ReadLine();
+                newIngredient.name = StringCheck();
                 Console.WriteLine("Please Enter the Unit of Measurement (whole/ml/cups/teaspoons)");
                 newIngredient.unit = Console.ReadLine();
                 Console.WriteLine("How Much/Many of the Ingredient Will You Use? (Don't type in the Unit of Meaasurement)");
-                newIngredient.quantity = Double.Parse(Console.ReadLine());
+                while (newIngredient.quantity <= 0)
+                {
+                    try
+                    {
+                        newIngredient.quantity = Double.Parse(Console.ReadLine());
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine($"{ex.Message}");
+                        Console.WriteLine("Please Type in a Number that is Greater than Zero. No Letters, Words or Phrases.");
+                        continue;
+                    }
+                }
                 Ingredients.Add(newIngredient);
             }
         }
@@ -64,6 +92,12 @@ namespace Prog6221_POE
             Console.WriteLine("Please Enter Step " + NumOfSteps);
             Step newStep = new Step();
             newStep.StepDescription = Console.ReadLine();
+            while (String.IsNullOrWhiteSpace(newStep.StepDescription)) 
+            {
+                Console.WriteLine("The Step Cannot Be Empty");
+                Console.WriteLine("Please Try Again");
+                newStep.StepDescription = Console.ReadLine();
+            }
             Steps.Add(newStep);
             while (newStep.StepDescription != "Finish")
             {
@@ -71,6 +105,12 @@ namespace Prog6221_POE
                 Console.WriteLine("Please Enter the Step " + NumOfSteps + " or Finish to Finish");
                 newStep = new Step();
                 newStep.StepDescription = Console.ReadLine();
+                while (String.IsNullOrWhiteSpace(newStep.StepDescription))
+                {
+                    Console.WriteLine("The Step Cannot Be Empty");
+                    Console.WriteLine("Please Try Again");
+                    newStep.StepDescription = Console.ReadLine();
+                }
                 Steps.Add(newStep);
             }
         }
@@ -78,13 +118,45 @@ namespace Prog6221_POE
         public void CreateRecipe()
         {
             Console.WriteLine("Please Enter A Name For Your Recipe");
-            RecipeName = Console.ReadLine();
+            RecipeName = StringCheck();
             Console.WriteLine("Please Enter The Number of Ingredients For Your Recipe");
-            NumOfIngredients = int.Parse(Console.ReadLine());
+            while (NumOfIngredients == 0)
+            {
+                try
+                {
+                    NumOfIngredients = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Please Type in a Number. No Letters, Words or Phrases.");
+                    continue;
+                }
+            }
             InputIngredients(NumOfIngredients);
             InputStep();
             Scale = 1;
             Recipes recipes = new Recipes(RecipeName, NumOfIngredients, Ingredients, Steps, NumOfSteps, Scale);
+        }
+
+        public string StringCheck()
+        {
+            string input = Console.ReadLine();
+            while (Regex.IsMatch(input, @"\d") || String.IsNullOrEmpty(input))
+            {
+                if (Regex.IsMatch(input, @"\d"))
+                {
+                    Console.WriteLine("Please Do Not Enter Any Numbers");
+
+                }
+                if (String.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Please Do Not Leave Empty");
+                }
+                Console.WriteLine("Please Try Again.");
+                input = Console.ReadLine();
+            }
+            return input;
         }
 
         public double GetScale()
@@ -92,30 +164,32 @@ namespace Prog6221_POE
             Console.WriteLine("Please Select How You Want To Scale The Recipe");
             Console.WriteLine("1. Half or 2. Double or 3. Triple");
             double scale = 1;
-                int input = int.Parse(Console.ReadLine());
-            while (true)
+            string input;
+            do
             {
+                input = Console.ReadLine();
                 switch (input)
                 {
-                    case 1:
+                    case "1":
                         scale = 0.5;
-                        return scale;
+                        break;
 
-                    case 2:
+                    case "2":
                         scale = 2;
-                        return scale;
+                        break;
 
-                    case 3:
+                    case "3":
                         scale = 3;
-                        return scale;
+                        break;
 
                     default:
                         Console.WriteLine("Please select Either Option 1 for Half, 2 for Double or 3 for Triple.");
-                        GetScale();
                         continue;
                 }
 
-            }
+            } while (input != "1" && input != "2" && input != "3" && input != null);
+
+            return scale;
         }
 
         public void ScaleRecipe(Recipes recipes, double scale)
@@ -131,9 +205,9 @@ namespace Prog6221_POE
         {
             Console.WriteLine("\nRecipe: " +
                 "\n" + RecipeName + ": " +
+                 "\nScale:" + Scale +
                 "\n" + NumOfIngredients + " Ingredients" +
-                "\nScale:" + Scale +
-                "\n\n" + "Ingredients: ") ;
+                "\n\n" + "Ingredients: ");
             foreach (var Ingredients in Ingredients)
             {
                 Console.WriteLine(Ingredients.ToString());
